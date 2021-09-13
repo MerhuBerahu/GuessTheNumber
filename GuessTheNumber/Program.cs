89a5358bedaa0f847cc5810c9c46699ea3a9a714
wmclaughlan <wmclaughlan@gmail.com>
2021-09-13 09:35:10 +0100

    commit

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -9,26 +9,24 @@ namespace GuessTheNumber
             Welcome();
         }
 
-        static void GameOver()
-        {
-            Console.WriteLine("Game Over!");
-        }
-
         public static void Welcome()
         {
             Console.WriteLine("Welcome to the Guess the Number Game");
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine("I'm thinking of a number between 1 and 100, you have 5 guesses");
+            int number = ChooseDifficulty();
 
-            int number = RandNumber();
-            bool won = false;
+
+            //Console.WriteLine($"I'm thinking of a number between {num1} and {num2}, you have 5 guesses");
+
+            
+            
             int tries = 0;
-            while ((won != true) || (tries < 5)) //keep looping while player hasnt won.
+            while ((tries < 5)) //keep looping while player hasnt won.
             {
                 Console.Write($"Guess number {tries + 1}: Guess a Number..");
                 System.Diagnostics.Debug.WriteLine($"tries = {tries}");
-                
+
                 int guess = Convert.ToInt32(Console.ReadLine());
                 if (guess < number)
                 {
@@ -41,21 +39,106 @@ namespace GuessTheNumber
                 else if (guess == number)
                 {
                     Console.WriteLine($"Congratulations, You guessed correctly, I was indeed thinking of {number}");
-                    won = true;
+                    return;
                 }
 
                 tries++;
             }
 
-            GameOver();
+            Console.WriteLine("You've lost!");
 
-            static int RandNumber()
+        }   
+                
+           
+
+
+        static public int ChooseDifficulty()
+        {
+            Console.WriteLine("Choose a Difficulty:\n");
+            Console.WriteLine("1 - Easy");
+            Console.WriteLine("2 - Medium");
+            Console.WriteLine("3 - Hard");
+            Console.WriteLine("4 - Insane");
+            string difficulty = Console.ReadLine();
+
+            int num = 0;
+            Int32.TryParse(difficulty, out num);
+
+            switch (num)
             {
-                Random RandomNumber = new System.Random();
-                return RandomNumber.Next(1, 100); //random num between 1 and 100
+                case 1:
+                    num = RandNumber(0, 10);
+                    return num; //return random number         
+
+                case 2:
+                    num = RandNumber(0, 100);
+                    return num;                   
+
+                case 3:
+                    num = RandNumber(0, 500);
+                    return num;
+
+                case 4:
+                    num = RandNumber(0, 1000);
+                    return num;
+
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    return ChooseDifficulty();
+                    
             }
-
-
         }
+
+        /*public static int ChooseDifficulty(out int Min, out int Max)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose a Difficulty:\n");
+                Console.WriteLine("1 - Easy");
+                Console.WriteLine("2 - Medium");
+                Console.WriteLine("3 - Hard");
+                Console.WriteLine("4 - Insane");
+
+                int difficulty = Convert.ToInt32(Console.ReadLine());
+
+                Min = 0;
+
+                switch (difficulty)
+                {
+                    case 1:
+                        Max = 10;
+                        return RandNumber(Min, Max);
+                    case 2:
+                        Max = 100;
+                        return RandNumber(Min, Max);
+
+                    case 3:
+                        Max = 500;
+                        return RandNumber(Min, Max);
+
+                    case 4:
+                        Max = 1000;
+                        return RandNumber(Min, Max);
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        continue;
+                }
+            }
+        }*/
+
+        static public int RandNumber(int num1, int num2)
+        {
+            Random RandomNumber = new System.Random();
+            return RandomNumber.Next(num1, num2); //random num between 1 and whatever. TODO return num1 and num2 to programmatically write limits of guess
+        }
+
+        static void GameOver()
+        {
+            Console.WriteLine("Game Over!");
+        }
+
+
     }
 }
+
